8e7a39b7ce05fbc57e69bfcc57d57321b3e623e6
Eyzarim <96677601+Eyzarim@users.noreply.github.com>
2024-09-08 14:32:35 +0800

    nest

--- new.py
+++ new.py
@@ -43,7 +43,7 @@ def perform_soft_tuning(model, tokenizer):
     training_args = TrainingArguments(
         output_dir="./results",
         learning_rate=3e-5,
-        per_device_train_batch_size=10,
+        per_device_train_batch_size=6,
         num_train_epochs=1,  # Sesuaikan jumlah epoch sesuai kebutuhan Anda
         save_steps=50000,
         logging_dir='./logs',
